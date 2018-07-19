951b6e500cb0180961fcae1ca442a69baa69e5a7
thraithep <thraithep.arincare@gmail.com>
2018-07-19 17:38:54 +0700

    [~]

---
+++ tests/EmailTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeCreatedFromValidEmailAddress()
+    {
+        $this->assertInstanceOf(
+            Email::class,
+            Email::fromString('user@example.com')
+        );
+    }
+
+    public function testCanBeUsedAsString()
+    {
+        $this->assertEquals(
+            'user@example.com',
+            Email::fromString('user@example.com')
+        );
+    }
+}
